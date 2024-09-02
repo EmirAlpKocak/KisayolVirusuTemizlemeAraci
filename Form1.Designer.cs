a94eb08166a18e25002dc3fd804db50c76e44c1f
EmirAlpKocak <168924880+EmirAlpKocak@users.noreply.github.com>
2024-09-02 15:37:24 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿
+namespace Kısayol_Virüsü_Temizleme_Aracı
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.driveListBox1 = new Microsoft.VisualBasic.Compatibility.VB6.DriveListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Temizlenecek Sürücü:";
+            // 
+            // driveListBox1
+            // 
+            this.driveListBox1.FormattingEnabled = true;
+            this.driveListBox1.Location = new System.Drawing.Point(15, 25);
+            this.driveListBox1.Name = "driveListBox1";
+            this.driveListBox1.Size = new System.Drawing.Size(110, 21);
+            this.driveListBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(15, 52);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Temizle";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(96, 52);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Hakkında";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(237, 83);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.driveListBox1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Kısayol Virüsü Temizleme Aracı";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private Microsoft.VisualBasic.Compatibility.VB6.DriveListBox driveListBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace Kısayol_Virüsü_Temizleme_Aracı
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (driveListBox1.Drive == "c:")
+            {
+                MessageBox.Show("C:\\ diski üzerinde kısayol virüsü temizleme işlemi yapılamaz. Bunu yapmak bilgisayarınızdaki tüm kısayolların silinmesine sebep olacaktır.", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                Clean();
+            }
+        }
+        private void Clean()
+        {
+            if (MessageBox.Show("Seçtiğiniz sürücüdeki tüm kısayollar ve autorun.inf dosyası silinecektir. Aynı zamanda gizli olan tüm dosyalar ve klasörler tekrar görülebilir olacaktır. Devam etmek istiyor musunuz?" , "Onay", MessageBoxButtons.YesNo , MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                try
+                {
+                    //Kısayol dosyalarını silelim.
+                    string drive = driveListBox1.Drive + "\\";
+                    ProcessStartInfo process1 = new ProcessStartInfo("cmd", "/c del " + drive + "*.lnk /s");
+                    process1.WindowStyle = ProcessWindowStyle.Hidden;
+                    Process.Start(process1);
+                    //Autorun dosyasını silelim.
+                    if (File.Exists(driveListBox1.Drive + "\\autorun.inf") == true)
+                    {
+                        File.Delete(drive + "autorun.inf");
+                    }
+                    //Virüsün gizlediği dosyaları tekrar görünür yapalım.
+                    process1.FileName = "attrib";
+                    process1.Arguments = "-s -r -h " + drive + "*.* /s /d";
+                    Process.Start(process1);
+                    MessageBox.Show("Virüs sürücüden başarıyla kaldırıldı.", "Temizle", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Virüs sürücüden kaldırılamadı. Hata: " + ex.Message, "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Kısayol Virüsü Temizleme Aracı 1.0 - DarkEmir", "Hakkında", MessageBoxButtons.OK);
+        }
+    }
+}
